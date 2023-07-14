8800a8a02246bec2502da2acb7d718d965dc243f
dlihawi <dlihawi14@gmail.com>
2023-07-14 08:57:32 +0300

    BMI SCALE

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("./", function(req, res){
+  res.sendFile(__dirname + "./calculator.html");
+});
+
+app.post("/", function(req, res){
+  const body = req.body;
+  const weight = Number(body.weight);
+  const height = Number(body.height);
+  const result = weight / (height * height);
+  res.send(`Your BMI result is ${result}`);
+});
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000.");
+});
