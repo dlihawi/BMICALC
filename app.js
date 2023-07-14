6d2d6e366632edda6e99d302b8ef2c1a5a415778
dlihawi <dlihawi14@gmail.com>
2023-07-14 09:37:43 +0300

    changes made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser")
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,5 +19,5 @@ app.post("/", function(req, res){
 });
 
 app.listen(3000, function(){
-  console.log("server is running on port 3000.");
+  console.log("server is running on port ${port}.");
 });
